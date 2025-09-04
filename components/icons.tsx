37965b521ccb8fac9b28e233001ff418d3b23448
Insightwithhari <113787584+Insightwithhari@users.noreply.github.com>
2025-09-04 18:00:03 +0530

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,4 +1,3 @@
-
 import type React from 'react';
 
 export const UserIcon: React.FC<{ className?: string }> = ({ className = "w-6 h-6" }) => (
@@ -23,5 +22,11 @@ export const DownloadIcon: React.FC<{ className?: string }> = ({ className = "w-
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className={className}>
         <path d="M10.75 2.75a.75.75 0 0 0-1.5 0v8.614L6.295 8.235a.75.75 0 1 0-1.09 1.03l4.25 4.5a.75.75 0 0 0 1.09 0l4.25-4.5a.75.75 0 0 0-1.09-1.03l-2.955 3.129V2.75Z" />
         <path d="M3.5 12.75a.75.75 0 0 0-1.5 0v2.5A2.75 2.75 0 0 0 4.75 18h10.5A2.75 2.75 0 0 0 18 15.25v-2.5a.75.75 0 0 0-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5Z" />
+    </svg>
+);
+
+export const MenuIcon: React.FC<{ className?: string }> = ({ className = "w-6 h-6" }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
     </svg>
 );
